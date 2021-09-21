930ff9413bf0e3b51574e0615fea8c3a2313a757
David Villamarin <77456654+dalejo96@users.noreply.github.com>
2021-09-20 21:57:09 -0500

    Cart and modal feature (#2)
    Co-authored-by: David Villamarin <dvillamarin@stackbuilders.com>

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,12 +6,12 @@ import styles from "./Header.module.css";
 
 import imageMeal from "../../assets/meals.jpg";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
         <h1>React Food App</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={styles["main-image"]}>
         <img src={imageMeal} alt="A table full of food" />
